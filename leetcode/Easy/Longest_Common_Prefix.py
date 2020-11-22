933b63d009a36320a3289901183161021c72b479
develop-hsw <poohpama@naver.com>
2020-11-22 18:28:11 +0900

    [2020-11-22]

--- leetcode/Easy/Longest_Common_Prefix.py
+++ leetcode/Easy/Longest_Common_Prefix.py
@@ -1,31 +1,9 @@
 strs = ["flower", "flow", "flight"]
-
+#strs = ["apple", "arrow", "awesome"]
+#strs = ["dogs", "racecar", "car"]
+#strs = ["a"]
+#strs = [""]
 def longestCommonPrefix(strs):
-    arrays = []
     answer = []
-
-    for i in range(0, len(strs)):
-        arrays.append(list(strs[i]))
-    print(arrays)
-
-    for i in range(0, len(arrays)-1):
-        if(len(arrays[i]) > len(arrays[i+1])):
-            length = len(arrays[i+1])
-        else:
-            length = len(arrays[i])
-
-    for i in range(0, length):
-        for j in range(0, len(arrays)-1):
-            prefix = arrays[0][i]
-            print(prefix)
-            if():
-                answer.append(prefix)
-            else:
-                break
-            print(prefix)
-
-    print(answer)
     return answer
-
-
 longestCommonPrefix(strs)
---
+++ leetcode/Easy/Merge_Two_Sorted_Lists.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Node:
+    def __init__(self, data, next=None):
+        self.datat = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self, data):
+        self.head = Node(data)
+    def add(self, data):
+        if self.head == "":
+            self.head = Node(data)
+        else:
+            node = self.head
+            while node.next:
+                node = node.next
+            node.next = None(data)
+
+def mergeTwoLists(list1, list2):
+
+mergeTwoLists(list1, list2)
