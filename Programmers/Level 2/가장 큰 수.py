d89bc306093eb462180d3fe0ca7bbbefc2e00bf2
develop-hsw <poohpama@naver.com>
2020-12-02 17:51:31 +0900

    [2020-12-02]

---
+++ Programmers/Level 2/가장 큰 수.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+
+list1 = [6, 2, 10]
+list2 = [3, 30, 34, 5, 9]
+
+def solution():
+    answer = []
+    temp = []
+    cases = list(permutations(list1, len(list1)))
+    cases = list(map(list, cases))
+    cases = list(map(str, map(list, cases)))
+    print(cases)
+    return 0
+
+solution()
+
+
+
+"""
+map 사용 :
+for idx in range(0, len(cases)):
+    cases[idx] = list(cases[idx])
+    
+-> cases = list(map(list, cases))
+"""
