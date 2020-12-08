832aa976c5cef575ecad97f6fd6a2ac01d2c1d52
hsw_1005 <43718769+develop-hsw@users.noreply.github.com>
2020-12-08 18:36:41 +0900

    [2020-12-08]

--- BOJ/1932.py
+++ BOJ/1932.py
@@ -13,3 +13,12 @@ for i in range(0, n):
 for i in range(0, n):
     for j in range(0, i):
         pass
+
+
+"""
+총 4개의 루트 :
+(i, j) + (i+1, j) + (i+2, j)
+(i, j) + (i+1, j) + (i+2, j+1)
+(i, j) + (i+1, j+1) + (i+2, j+1)
+(i, j) + (i+1, j+2) + (i+2, j+2)
+"""
