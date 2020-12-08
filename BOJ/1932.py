bbf8460c6118a6b6604aed3082f419af7c22919e
hsw_1005 <43718769+develop-hsw@users.noreply.github.com>
2020-12-08 15:59:43 +0900

    [2020-12-08]

--- BOJ/1932.py
+++ BOJ/1932.py
@@ -1,12 +1,12 @@
 import sys
 input = sys.stdin.readline
 
+# 입력 및 초기화
 n = int(input())
 tri = [[0]*n for i in range(n)]
-
-print(tri)
 
 for i in range(0, n):
     tri[i] = list(map(int, input().split()))
 
-print(tri)
+for i in range(0, n):
+    for j in range(0, i):
