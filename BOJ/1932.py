9fb8c1809e3ef895b2f0c2828117cfe04eeab427
develop-hsw <poohpama@naver.com>
2020-12-09 07:31:01 +0900

    [2020-12-09]

--- BOJ/1932.py
+++ BOJ/1932.py
@@ -6,14 +6,27 @@ input = sys.stdin.readline
 # 입력 및 초기화
 n = int(input())
 tri = [[0]*n for i in range(n)]
+answer = []
 
 for i in range(0, n):
     tri[i] = list(map(int, input().split()))
 
-for i in range(0, n):
-    for j in range(0, i):
-        pass
+print(tri)
+a = tri[0][0]
+b = tri[0][0]
+c = a
+d = b
+e = 0
+for i in range(0, n+1):
+    for j in range(0, i+1):
+        print("({0}, {1})".format(i, j))
+        a = tri[i][j] + tri[i+1][j]
+        b = tri[i][j] + tri[i+1][j+1]
 
+        answer.append(a)
+        answer.append(b)
+
+print(answer)
 
 """
 총 4개의 루트 :
@@ -21,4 +34,13 @@ for i in range(0, n):
 (i, j) + (i+1, j) + (i+2, j+1)
 (i, j) + (i+1, j+1) + (i+2, j+1)
 (i, j) + (i+1, j+2) + (i+2, j+2)
+"""
+
+"""
+5
+7
+3 8
+8 1 0
+2 7 4 4
+4 5 2 6 5
 """
