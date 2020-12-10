3ecb2478f563de550f539532cb40826a86f7b6ca
develop-hsw <poohpama@naver.com>
2020-12-10 13:19:46 +0900

    [2020-12-10]

--- BOJ/11053.py
+++ BOJ/11053.py
@@ -11,18 +11,7 @@ max = 0
 
 
 for i in range(0, len(arr)-1):
-    if(i == 0):
-        if(arr[i] < arr[i+1]):
-            temp.append(arr[i])
-            temp.append(arr[i+1])
-        else:
-            temp.append(arr[i+1])
-            temp.append(arr[i])
-    else:
-        if(arr[i+1] > arr[i]):
-            temp.append(arr[i+1])
-        else:
-            continue
+
 
 
 print(temp)
