bd5972897b05759329bf5219e228af955cc6bb48
hsw_1005 <poohpama@naver.com>
2020-12-30 17:14:02 +0900

    [2020-12-30]

--- leetcode/Easy/Longest_Common_Prefix.py
+++ leetcode/Easy/Longest_Common_Prefix.py
@@ -1,11 +1,3 @@
-# re
-
 strs = ["flower", "flow", "flight"]
-#strs = ["apple", "arrow", "awesome"]
-#strs = ["dogs", "racecar", "car"]
-#strs = ["a"]
-#strs = [""]
-def longestCommonPrefix(strs):
-    answer = []
-    return answer
-longestCommonPrefix(strs)
+strs2 = ["dog", "racecar", "car"]
+
--- leetcode/Easy/Valid_Parentheses.py
+++ leetcode/Easy/Valid_Parentheses.py
@@ -1,1 +1,36 @@
-# re
+l1 = [1, 2, 4]
+l2 = [1, 3, 4]
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+
+    def addToList(self, newData):
+        newNode = Node(newData)
+        if self.head is None:
+            self.head = newNode
+            return
+
+        last = self.head
+        while last.next:
+            last = last.next
+
+        last.next = newNode
+
+def mergeLists(headA, headB):
+    dummyNode = Node(0)
+
+    tail = dummyNode
+    while True:
