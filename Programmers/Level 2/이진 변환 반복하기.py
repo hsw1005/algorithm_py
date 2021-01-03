8d363145d6f90073bb3a6a1d33dcaca1d0f107fd
hsw_1005 <poohpama@naver.com>
2021-01-03 14:23:34 +0900

    [2020-01-03]

--- Programmers/Level 2/이진 변환 반복하기.py
+++ Programmers/Level 2/이진 변환 반복하기.py
@@ -1,38 +1,11 @@
 def solution(s):
-    answer = []
-    sum = 0
-    phase = 0
-    for i in range(10):
-        s, count = recursive(s)
-        sum += count
-        phase += 1
-        if(len(s) == 1):
-            break
+    answer = [0, 0]
 
-        print(s, count)
-        print(sum)
-        print('phase', phase)
-
-    answer.append(phase)
-    answer.append(count)
-
-    return answer
-
-
-def recursive(s):
-    temp = []
-    count = 0
-    for i in range(0, len(s)):
-        temp.append(s[i])
-
-    for j in range(0, len(temp)):
-        if temp[j] == '0':
-            del (temp[j])
-            count+=1
-
-    return temp, count
+    while s != '1':
+        answer[1] += s.count("0")
+        s = bin(s.count('1'))[2:]
+        answer[0] += 1
+    print(answer)
 
 s = "110010101001"
-answer = solution(s)
-print(answer)
-print(answer)
+solution(s)
