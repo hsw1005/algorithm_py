54a91a20be6fe56ebbd6e3addc1d0b3e746ee612
hsw_1005 <poohpama@naver.com>
2021-02-18 23:54:19 +0900

    [2021-02-15]

--- BOJ/tester.py
+++ BOJ/tester.py
@@ -1,38 +1,22 @@
-from collections import deque
-cmd = deque()
-t = int(input())
+import sys
 
-for i in range(t):
-    a, b, c = map(int, input().split())
-    print("a, b, c", a, b, c)
-    for i in range(c):
-        cmd.append(list(map(int, input().split())))
+input = sys.stdin.readline
 
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
 
-    print(cmd)
-    cmd =[]
+arr.sort()
 
+for i in range(0, len(arr)):
+    print(arr[i])
 
 """
+5
+5
+4
+3
 2
-10 8 17
-0 0
-1 0
-1 1
-4 2
-4 3
-4 5
-2 4
-3 4
-7 4
-8 4
-9 4
-7 5
-8 5
-9 5
-7 6
-8 6
-9 6
-10 10 1
-5 5
+1
 """
