7b90e95aa89f655b7030cf6de4521ec6d26add9b
develop-hsw <poohpama@gmail.com>
2021-04-06 17:39:56 +0900

    [2021-04-06]

--- 이취코테/교재/13. DFS & BFS/2. 문제/미로 탈출.py
+++ 이취코테/교재/13. DFS & BFS/2. 문제/미로 탈출.py
@@ -1,35 +1,34 @@
-# dfs
+from collections import deque
+
 n, m = map(int, input().split())
 
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
-exit = (n-1, m-1)
-print(exit)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def dfs(x, y):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
 
-    if graph[x][y] == 1:
-        graph[x][y] = 2
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y]+1
+                queue.append((nx, ny))
 
-        dfs(x-1, y)
-        dfs(x, y-1)
-        dfs(x+1, y)
-        dfs(x, y+1)
-        return True
+    return graph[n-1][m-1]
 
-
-    return False
-
-for i in range(n):
-    for j in range(m):
-        dfs(i, j)
-
-for i in range(0, len(graph)):
-    print(graph[i])
+print(bfs(0, 0))
 
 """
 5 6
--- 이취코테/교재/13. DFS & BFS/2. 문제/음료수 얼려 먹기.py
+++ 이취코테/교재/13. DFS & BFS/2. 문제/음료수 얼려 먹기.py
@@ -5,7 +5,7 @@ for i in range(n):
     graph.append(list(map(int, input())))
 
 def dfs(x, y):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
+    if x < 0 or x >= n or y < 0 or y >= m:
         return False
 
     if graph[x][y] == 0:
@@ -19,13 +19,13 @@ def dfs(x, y):
     return False
 
 result = 0
-
 for i in range(n):
     for j in range(m):
         if dfs(i, j) == True:
             result += 1
 
 print(result)
+
 
 """
 15 14
