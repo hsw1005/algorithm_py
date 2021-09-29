8cd5cfe263ad7c1ddbe585590737171d51cc92fa
hsw_1005 <poohpama@naver.com>
2021-09-29 22:55:20 +0900

    [프로그래머스] 정렬

---
+++ Programmers/Level 2/K번째 수.py
@@ -0,0 +1,12 @@
+def solution(array, commands):
+    answer = []
+
+    while len(commands) != 0:
+        arr = array
+        command = commands.pop(0)
+        short_arr = arr[command[0] - 1:command[1]]
+        short_arr.sort()
+        number = short_arr[command[2] - 1]
+        answer.append(number)
+
+    return answer
