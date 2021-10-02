d871f612a904de34bcc74dbc08f2b222dbcd2cd0
hsw_1005 <poohpama@naver.com>
2021-10-02 21:56:42 +0900

    2021.10.02

---
+++ Programmers/Level 1/1주차_부족한 금액 계산하기.py
@@ -0,0 +1,12 @@
+def solution(price, money, count):
+    answer = -1
+
+    for i in range(1, count + 1):
+        money -= price * i
+
+    if money < 0:
+        answer = -1 * money
+    else:
+        answer = 0
+
+    return answer
