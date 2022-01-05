64a38764e3f9b0c313bf71935865d84a30864c19
hsw1005 <poohpama@naver.com>
2022-01-05 14:38:20 +0900

    [inflearn]

--- 인프런/2. 코드 구현력 기르기/점수계산.py
+++ 인프런/2. 코드 구현력 기르기/점수계산.py
@@ -2,3 +2,15 @@
 10
 1 0 1 1 1 0 0 1 1 0
 """
+
+n = int(input())
+arr = list(map(str, input().split()))
+arr = "".join(arr).split("0")
+answer = 0
+
+for one in arr:
+    if one != '':
+        l = len(one)
+        answer += sum([i for i in range(1, l+1)])
+
+print(answer)
