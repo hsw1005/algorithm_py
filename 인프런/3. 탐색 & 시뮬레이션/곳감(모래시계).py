7e465b8a0e26d147b2d7aedcdf3ff1edc0ce5148
hsw_1005 <poohpama@naver.com>
2022-01-09 22:38:23 +0900

    [inflearn]

---
+++ 인프런/3. 탐색 & 시뮬레이션/곳감(모래시계).py
@@ -0,0 +1,31 @@
+"""
+5
+10 13 10 12 15
+12 39 30 23 11
+11 25 50 53 15
+19 27 29 37 27
+19 13 30 13 19
+3
+2 0 3
+5 1 2
+3 1 4
+"""
+
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+m = int(input())
+commands = [list(map(int, input().split())) for _ in range(m)]
+
+for command in commands:
+    if command[1] == 0: #왼쪽
+        for _ in range(command[2]):
+            matrix[command[0]-1] = matrix[command[0]-1][1:] + matrix[command[0]-1][:1]
+    if command[1] == 1: #오른쪽
+        for _ in range(command[2]):
+            matrix[command[0]-1] = matrix[command[0]-1][-1:] + matrix[command[0]-1][:-1]
+
+l = len(matrix)
+mid = n // 2
+for i in range(l):
+    for j in range(l):
+        
