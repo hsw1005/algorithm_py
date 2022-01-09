87e47c986a9a26b4031c1766d4e0d5e6b8a93e93
hsw1005 <poohpama@naver.com>
2022-01-09 17:34:58 +0900

    [inflearn]

--- tester.py
+++ tester.py
@@ -1,76 +1,42 @@
-# 최소 인원수 n = 4, 최소 입학 연도 수 m = 3, 최소 학과 수 k = 3
+"""
+5
+10 13 10 12 15
+12 39 30 23 11
+11 25 50 53 15
+19 27 29 37 27
+19 13 30 13 19
 
-arrs = ["13123820 BusinessManagement AAA",
-       "15047648 Economics AAA",
-       "17869244 ComputerScience AAA",
-       "19000000 ElectronicEngineering AAA",
-       "14281264 Law AAA",
-       "19000000 ElectronicEngineering BBB",
-       "16188768 BusinessManagement BBB",
-       "14634411 ElectronicEngineering BBB",
-       "16628760 Economics BBB",
-       "14863606 BusinessManagement CCC",
-       "13165700 Law CCC",
-       "15215218 ComputerScience CCC"]
+"""
+#mid와 i의 거리를 활용한다?
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range()]
+mid = n // 2 # mid == 2
+answer = 0
+for i in range(n):
+    for j in range(n):
+        if i <= mid:
+            for k in range(mid-i, mid+i+1):
+                answer += arr[i][k]
+        if i > mid:
+            #for k in range(n-mid+1, i-mid-1, -1):
+            for k in range(i-mid, n-i+mid):
+                answer += arr[i][k]
+            # if mid <= i+j <= mid*3:
+            #     arr[i][j] = 1
 
-def solution(arr, n, m, k): # 0 : AAA, 1 : BBB, 2 : CCC
-    answer = 0
-
-    clubs = []
-    for arr in arrs:
-        club = arr.split(" ")[2]
-        if club not in clubs:
-            clubs.append(club)
-
-    stu_id_list = [[club] for club in clubs]
-    class_list = [[club] for club in clubs]
-    year_list = [[club] for club in clubs]
-
-    for i, club in enumerate(clubs):
-        for arr in arrs:
-            stu_id, class_id, stu_club = arr.split(" ")
-            year = stu_id[0:2]
-            if club == stu_club:
-                stu_id_list[i].append(stu_id)
-                class_list[i].append(class_id)
-                year_list[i].append(year)
-
-    check = [-1, -1, -1]
-    commons = []
-    for i in range(0, len(stu_id_list)):
-        for j in range(i+1, len(stu_id_list)):
-            commons.append(list(set(stu_id_list[i]).intersection(stu_id_list[j])))
-
-    for common in commons:
-        if len(common) != 0:
-            for i in range(0, len(stu_id_list)):
-                if common[0] in stu_id_list[i]:
-                    idx = stu_id_list[i].index(common[0])
-                    stu_id_list[i].pop(idx)
-                    class_list[i].pop(idx)
-                    year_list[i].pop(idx)
-                if len(stu_id_list[i])-1 >= n:
-                    check[0] = 1
-                if len(list(set(year_list[i])))-1 >= m:
-                    check[1] = 1
-                if len(list(set(class_list[i])))-1 >= k:
-                    check[2] = 1
-
-                if sum(check) == 3:
-                    answer += 1
-                    break
-        else:
-            check = [-1, -1, -1]
-            continue
-
-    print(answer)
-    print(stu_id_list)
-    print(class_list)
-    print(year_list)
-
-    #list(set(a).intersection(b))
+print(answer)
 
 
-n, m, k = 4, 3, 3
-solution(arrs, n, m, k)
+
+
+""" [i][j] : distance = -2, -1, 0, 1, 2
+
+00 01 __ 03 04  |  __ __ 02 __ __  |  -2  |  0 1 2 3 4
+10 __ __ __ 14  |  __ 11 12 13 __  |  -1  |  1 2 3 4 5
+__ __ __ __ __  |  20 21 22 23 24  |   0  |  2 3 4 5 6
+30 __ __ __ 34  |  __ 31 32 33 __  |   1  |  3 4 5 6 7
+40 41 __ 43 44  |  __ __ 42 __ __  |   2  |  4 5 6 7 8
+
+-> 2 | 2 3 4 | 2 3 4 5 6 | 4 5 6 | 6 
+"""
 
