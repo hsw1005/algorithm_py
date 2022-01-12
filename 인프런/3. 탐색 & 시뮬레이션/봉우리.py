c1525ac28c3a3735967f673a9418f0a7a117e94b
hsw1005 <poohpama@naver.com>
2022-01-12 17:16:56 +0900

    [inflearn]

--- 인프런/3. 탐색 & 시뮬레이션/봉우리.py
+++ 인프런/3. 탐색 & 시뮬레이션/봉우리.py
@@ -8,3 +8,25 @@
 
 -> 10
 """
+
+n = int(input())
+arr = []
+zeros = [0] * (n+2)
+arr.append(zeros)
+for _ in range(n):
+    inp = list(map(int, input().split()))
+    inp.insert(0, 0)
+    inp.append(0)
+    arr.append(inp)
+arr.append(zeros)
+
+answer = 0
+
+for i in range(1, len(arr)-1):
+    for j in range(1, len(arr)-1):
+        if arr[i][j] > arr[i+1][j] and arr[i][j] > arr[i-1][j] and arr[i][j] > arr[i][j+1] and arr[i][j] > arr[i][j-1]:
+            answer += 1
+        else:
+            continue
+
+print(answer)
